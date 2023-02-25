a3faf2dac13f38ca34e9be6b77feee404473f4ad
miaroshi <70836575+miaroshi@users.noreply.github.com>
2023-02-25 15:44:13 +0100

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,9 @@
+aftman init
+aftman add rojo-rbx/rojo
+aftman add UpliftGames/wally
+aftman add Kampfkarren/selene
+aftman add johnnymorganz/stylua
+aftman install
+wally init
+rojo build -o build.rbxlx
+PAUSE
